4f739d48904f1fdf3e54f3b2d9ed4b5054b6036c
BORA <70184893+rara-record@users.noreply.github.com>
2023-06-30 17:12:36 +0900

    fix: package.json

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,24 +1,27 @@
-//See https://github.com/vercel/next.js/tree/canary/examples/with-jest
+// See https://github.com/vercel/next.js/tree/canary/examples/with-jest
 
 // Used for __tests__
 // Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@testing-library/jest-dom/extend-expect';
 
 // Polyfill "window.fetch" used in the React component.
-import "whatwg-fetch";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'whatwg-fetch';
 
 // Extend Jest "expect" functionality with Testing Library assertions.
-import "@testing-library/jest-dom";
-import { server } from "./tests/server";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@testing-library/jest-dom';
+import { server } from './tests/server';
 
 beforeAll(() => {
-    server.listen();
+  server.listen();
 });
 
 afterEach(() => {
-    server.resetHandlers();
+  server.resetHandlers();
 });
 
 afterAll(() => {
-    server.close();
+  server.close();
 });

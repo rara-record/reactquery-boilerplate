3380a6b4d3d0fe4b6863cf11b4c14791de4fffc7
BORA <70184893+rara-record@users.noreply.github.com>
2023-06-30 17:12:19 +0900

    feat: eslint comment

--- tests/utils.tsx
+++ tests/utils.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from '@testing-library/react';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider, setLogger } from 'react-query';
